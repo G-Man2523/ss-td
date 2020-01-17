012ac7e5d6e338c679649f12a1c7ada81b849b32
Muhammet Dilek <dilekmuhammet@gmail.com>
2020-01-17 18:34:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const fs = require('fs');
+let files = fs.readdirSync('.');
+
 console.log("grab all files...");
+console.log(files);
 console.log("push all files to api...");
 console.log("push searchable file contents to api...");
